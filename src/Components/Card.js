3d6611dd9256f46023c26ca5a8d9bc05a0491114
sonali kokalki <sonalikokalki@gmail.com>
2023-07-01 19:38:47 +0530

    Fix: fixed the bug

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,22 +1,28 @@
-export default function Card(props){
-    let badgeText
-    if(props.item.openSpots === 0){
-        badgeText = "SOLD OUT"
-    }else if (props.item.location === "Online"){
-        badgeText = "ONLINE"
-    }
-    return(
-        <div className="card">
-            { badgeText && <div className="card--badge">{badgeText}</div>}
-        <img src={props.item.coverImg}className="card--image" />
-        <div className="card--stats">
-            <img src="../images/star.png" className="card--star" alt="star image"/>
-            <span>{props.item.stats.rating}</span>
-            <span className="gray">({props.item.stats.reviewCount}) • </span>
-            <span className="gray">{props.item.location}</span>
-        </div>
-        <p>{props.item.title}</p>
-        <p><span className="bold">From ${props.item.price}</span> / person</p>
+export default function Card(props) {
+  let badgeText;
+  if (props.item.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.item.location === "Online") {
+    badgeText = "ONLINE";
+  }
+  return (
+    <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img
+        src={props.item.coverImg}
+        className="card--image"
+        alt="badge image"
+      />
+      <div className="card--stats">
+        <img src="../images/star.png" className="card--star" alt="star image" />
+        <span>{props.item.stats.rating}</span>
+        <span className="gray">({props.item.stats.reviewCount}) • </span>
+        <span className="gray">{props.item.location}</span>
+      </div>
+      <p>{props.item.title}</p>
+      <p>
+        <span className="bold">From ${props.item.price}</span> / person
+      </p>
     </div>
-    )
+  );
 }
--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-export default data = [
+export default [
   {
     id: 1,
     title: "Life Lessons with Katie Zaferes",
